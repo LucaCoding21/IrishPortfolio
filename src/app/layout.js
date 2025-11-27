dec95b117ccca823e9fadf1fe6dd1fa226096188
Irish Catungal <irishclaireparayno@gmail.com>
2025-11-27 14:23:30 -0800

    deployment bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,12 +11,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300..900&family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
-          rel="stylesheet"
-        />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
